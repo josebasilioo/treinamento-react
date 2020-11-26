c934a04cb7ef911929cf460b898faebb1904be44
Gabriel Melo <xgabrielgomesmelo@gmail.com>
2020-11-25 23:17:59 -0300

    feature(base-keystone): edit banners model

--- server/models/banner.js
+++ server/models/banner.js
@@ -3,18 +3,24 @@ const keystone = require('keystone');
 const { Types } = keystone.Field;
 
 const banner = new keystone.List('banner', {
-    map: { name: 'image'},
+    map: { name: 'key'},
     nocreate: true,
     nodelete: true,
+    unique: true
 });
 
 banner.add({
+    key: {
+        type: Types.Text,
+        value: 'Banner',
+        noedit: true
+    },
     image: {
         type: Types.CloudinaryImage,
         required: true,
         initial: true,
         index: true,
-        unique: true,
+        unique: true
     }
 });
 
