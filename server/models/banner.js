af65bf769e9953eeff3d91d7e4c23584f22b3294
Gabriel Melo <xgabrielgomesmelo@gmail.com>
2020-11-26 11:50:21 -0300

    feature(base-keystone): allows banners editing

--- server/models/banner.js
+++ server/models/banner.js
@@ -3,19 +3,24 @@ const keystone = require('keystone');
 const { Types } = keystone.Field;
 
 const banner = new keystone.List('banner', {
-    map: { name: 'image'},
+    map: { name: 'key' },
     nocreate: true,
     nodelete: true,
+    unique: true
 });
 
 banner.add({
+    key: {
+        type: Types.Text,
+        value: 'Banner',
+        noedit: true
+    },
     image: {
         type: Types.CloudinaryImage,
         required: true,
+        unique: true,
         initial: true,
         index: true,
-        unique: true,
-        noedit: false
     }
 });
 
