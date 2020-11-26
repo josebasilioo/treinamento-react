b73047795b1b8b5ff97dccbcc388619915a66d0e
Felipe <jfss2@cin.ufpe.br>
2020-11-26 16:24:19 -0300

    feature(social-media): create the social media model

---
+++ server/models/RedesSociais.js
@@ -0,0 +1,48 @@
+const keystone = require("keystone")
+
+const { Types } = keystone.Field
+
+const redesSociais = new keystone.List("redesSociais", {
+    map: { name: "key" },
+    unique: true,
+    nodelete: true
+})
+
+redesSociais.add({
+    key: {
+        type: Types.Text,
+        value: "Redes sociais",
+        noedit: true
+    },
+    instagram: {
+        type: Types.Text,
+        required: true,
+        unique: true,
+        initial: true,
+        index: true
+    },
+    youtube: {
+        type: Types.Text,
+        required: true,
+        unique: true,
+        initial: true,
+        index: true
+    },
+    email: {
+        type: Types.Text,
+        required: true,
+        unique: true,
+        initial: true,
+        index: true
+    },
+    instagram: {
+        type: Types.Text,
+        required: true,
+        unique: true,
+        initial: true,
+        index: true
+    }
+
+})
+
+redesSociais.register()
