d684e98c895777897600c523616057ded2cd2be1
Gabriel Melo <xgabrielgomesmelo@gmail.com>
2020-11-27 00:12:28 -0300

    feature(partners): create partners model

---
+++ server/models/parceiros.js
@@ -0,0 +1,30 @@
+const keystone = require('keystone');
+
+const { Types } = keystone.Field;
+
+const parceiros = new keystone.List("parceiros", {
+    map: { name: "key" },
+    unique: true
+});
+
+parceiros.add({
+    key: {
+        type: Types.Text,
+        value: "Parceiros",
+        noedit: true
+    },
+    image: {
+        type: Types.CloudinaryImage,
+        required: true,
+        unique: true,
+        initial: true,
+        index: true
+    },
+    link: {
+        type: Types.Url,
+        required: true,
+        unique: true,
+        initial: true,
+        index: true
+    }
+})
